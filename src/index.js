427b6966c1c863ab7a77679ab21bb615bce1032a
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-29 21:15:57 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+let breedNames = []
+function getImage() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then((response)=> {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        for (let i = 0; i < data.message.length; i++) {
+          let imageDiv = document.getElementById("dog-image-container");
+          let image = document.createElement("img");
+          image.id = "img";
+          imageDiv.appendChild(image);
+          img.src = data.message[i];
+        }
+      });
+  }
+getImage()
+
+function getBreed() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((response)=> {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        for (breed in data.message) {
+            let nameDiv = document.getElementById("dog-breeds");
+            let list = document.createElement("li");
+            list.innerText = breed;
+            nameDiv.appendChild(list);
+            breedNames.push(breed);
+            list.addEventListener("click", () => {
+              list.style.color = "blue";
+            });
+          }
+      });
+  }
+getBreed()
+
